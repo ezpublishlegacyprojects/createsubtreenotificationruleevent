2bda85979e804b0b12335077314b58360c8134f3
brookinsconsulting <info@brookinsconsulting.com>
2008-02-27 05:03:57 +0000

    Added: Stable 1.0 branch
    git-svn-id: http://svn.projects.ez.no/createsubtreenotificationruleevent@1610 1d364a9f-59fd-4c7f-8892-9112f1565502

---
+++ stable/1.0/extension/sckcreatesubtreenotificationruleevent/ezinfo.php
@@ -0,0 +1,15 @@
+<?php
+class sckcreatesubtreenoficationruleeventInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "Create subtree notification rule event type",
+            'Version' => "1.0",
+            'Copyright' => "Copyright (C) 2006 SCK-CEN",
+            'Author' => "Kristof Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
